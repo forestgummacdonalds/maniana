06db4d469e68cd509059b2f31af98d53116fc7bf
forestgummacdonalds <yoolvema@gmail.com>
2024-07-09 21:26:36 -0500

    version prueba

---
+++ src/test/java/com/yns/maniana/ManianaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yns.maniana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManianaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
